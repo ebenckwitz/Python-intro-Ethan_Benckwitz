4a1aafb171c3ac09b1148b5421a3b6b45aae9314
Ethan <ebenckwitz>
2020-09-01 17:41:05 -0600

    fixed few things

--- wordCount.py
+++ wordCount.py
@@ -3,30 +3,17 @@
 #Date: 8/30/2020
 #Class: Theory of Operating Systems
 #Assignment 0: Python Intro
-#This assignment reads a given text file and counts the total number oftimes each
-#word is in the given file. We then print the occurences in alphabetical order in a new file
+#This assignment reads a given text file and counts the total number of times each
+#word is given in the file. We then print the occurences in alphabetical order in a new file.
 
 import sys          # command line arguments
 import re           # regular expression tools
-import os           # checking if file exists
-
-# this will set the input and output files
-if len(sys.argv) is not 3:
-    print("Correct usage: wordCount.py <input text file> <output file>")
-    exit()
 
 textFileName = sys.argv[1]
 outputFileName = sys.argv[2]
-
-#making sure the text file exists
-if not os.path.exists(textFileName):
-    print("text file input %s does not exist! Leaving the program." % textFileName)
-    exit()
     
-'''
-open the input file and remove puncuation while making everything lower case 
-and storing it in numWords
-'''
+
+#open the input file and remove punctuation while storing it in numWords
 inputFile = open(textFileName, "r")
 numWords = dict()
 for line in inputFile:
@@ -42,9 +29,7 @@ for line in inputFile:
             numWords[word] = 0
         numWords[word] += 1
 
-'''
-create an ouptut file and print out all words and their occurences
-'''
+#create an ouptut file and print out all words and their occurences'
 outputFile = open(outputFileName, "w")
 for key, count in sorted(numWords.items()):
     outputFile.write(str(key) + " " + str(count) + "\n")
